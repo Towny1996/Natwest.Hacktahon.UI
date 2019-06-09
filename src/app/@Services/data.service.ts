b9e3929038d11bcf902f69f7e2c8f35b8f6bda5d
Annas <abuhassan.annas@gmail.com>
2019-06-09 11:40:06 +0100

    css

--- src/app/@Services/data.service.ts
+++ src/app/@Services/data.service.ts
@@ -1,24 +1,47 @@
-import { Injectable } from '@angular/core';
-import { Transaction } from '../@Models/transaction.model';
-import { TransactionType } from '../@Enums/transaction-type.enum';
-import { FeeType } from '../@Enums/fee-type.enum';
+import { Injectable } from "@angular/core";
+import { Transaction } from "../@Models/transaction.model";
+import { TransactionType } from "../@Enums/transaction-type.enum";
+import { FeeType } from "../@Enums/fee-type.enum";
 import * as moment from "moment";
-import { TransactionItem } from '../@Models/transaction-item.model';
+import { TransactionItem } from "../@Models/transaction-item.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DataService {
-
   crapData = [
-    ["2019-06-02T02:59:43.1Z", "Dbit",100.00, "TRF","Account1", false,""],
-    ["2019-06-02T02:59:43.1Z", "Dbit",100.00, "TRF","Account1", true,"FEE DR"],
+    ["2019-06-02T02:59:43.1Z", "Dbit", 100.0, "TRF", "Account1", false, ""],
+    [
+      "2019-06-02T02:59:43.1Z",
+      "Dbit",
+      100.0,
+      "TRF",
+      "Account1",
+      true,
+      "FEE DR"
+    ],
 
-    ["2019-05-03T02:59:43.1Z", "CRDT",100.00, "TRF","Account1", false,"",""],
-    ["2019-05-03T02:59:43.1Z", "DBIT",100.00, "TRF","Account1", true,"FEE CR"],
+    ["2019-05-03T02:59:43.1Z", "CRDT", 100.0, "TRF", "Account1", false, "", ""],
+    [
+      "2019-05-03T02:59:43.1Z",
+      "DBIT",
+      100.0,
+      "TRF",
+      "Account1",
+      true,
+      "FEE CR"
+    ],
 
-    ["2019-06-03T02:59:43.1Z", "DBIT",100.00, "IPO","Account1", false,""],
-    ["2019-06-03T02:59:43.1Z", "DBIT",100.00, "IPO","Account1", true,"FEE DR"],
+    ["2019-06-03T02:59:43.1Z", "DBIT", 100.0, "IPO", "Account1", false, ""],
+    [
+      "2019-06-03T02:59:43.1Z",
+      "DBIT",
+      100.0,
+      "IPO",
+      "Account1",
+      true,
+      "FEE DR"
+    ],
 
     ["2019-05-01T02:59:43.1Z", "CRDT", 100.0, "IPO", "Account1", false, ""],
     [
@@ -31,8 +54,16 @@ export class DataService {
       "FEE CR"
     ],
 
-    ["2019-06-04T02:59:43.1Z", "DBIT",100.00, "PAY","Account1", false,""],
-    ["2019-06-04T02:59:43.1Z", "DBIT",100.00, "PAY","Account1", true,"FEE DR"],
+    ["2019-06-04T02:59:43.1Z", "DBIT", 100.0, "PAY", "Account1", false, ""],
+    [
+      "2019-06-04T02:59:43.1Z",
+      "DBIT",
+      100.0,
+      "PAY",
+      "Account1",
+      true,
+      "FEE DR"
+    ],
 
     ["2019-05-01T02:59:43.1Z", "CRDT", 100.0, "PAY", "Account1", false, ""],
     [
@@ -56,8 +87,16 @@ export class DataService {
       "FEE DR"
     ],
 
-    ["2019-06-05T02:59:43.1Z", "CRDT",100.00, "1855","Account1", false,""],
-    ["2019-06-05T02:59:43.1Z", "DBIT",100.00, "1855","Account1", true,"FEE CR"],
+    ["2019-06-05T02:59:43.1Z", "CRDT", 100.0, "1855", "Account1", false, ""],
+    [
+      "2019-06-05T02:59:43.1Z",
+      "DBIT",
+      100.0,
+      "1855",
+      "Account1",
+      true,
+      "FEE CR"
+    ],
 
     ["2019-05-01T02:59:43.1Z", "DBIT", 100.0, "SPIT", "Account1", false, ""],
     [
@@ -70,20 +109,60 @@ export class DataService {
       "FEE DR"
     ],
 
-    ["2019-05-06T02:59:43.1Z", "CRDT",100.00, "SPIT","Account1", false,""],
-    ["2019-05-06T02:59:43.1Z", "DBIT",100.00, "SPIT","Account1", true,"FEE CR"],
+    ["2019-05-06T02:59:43.1Z", "CRDT", 100.0, "SPIT", "Account1", false, ""],
+    [
+      "2019-05-06T02:59:43.1Z",
+      "DBIT",
+      100.0,
+      "SPIT",
+      "Account1",
+      true,
+      "FEE CR"
+    ],
 
-    ["2019-06-06T02:59:43.1Z", "DBIT",100.00, "MMD","Account1", false,""],
-    ["2019-06-06T02:59:43.1Z", "DBIT",100.00, "MMD","Account1", true,"FEE DR"],
+    ["2019-06-06T02:59:43.1Z", "DBIT", 100.0, "MMD", "Account1", false, ""],
+    [
+      "2019-06-06T02:59:43.1Z",
+      "DBIT",
+      100.0,
+      "MMD",
+      "Account1",
+      true,
+      "FEE DR"
+    ],
 
-    ["2019-06-07T02:59:43.1Z", "CRDT",100.00, "MMD","Account1", false,""],
-    ["2019-06-07T02:59:43.1Z", "DBIT",100.00, "MMD","Account1", true,"FEE CR"],
+    ["2019-06-07T02:59:43.1Z", "CRDT", 100.0, "MMD", "Account1", false, ""],
+    [
+      "2019-06-07T02:59:43.1Z",
+      "DBIT",
+      100.0,
+      "MMD",
+      "Account1",
+      true,
+      "FEE CR"
+    ],
 
-    ["2019-06-08T02:59:43.1Z", "DBIT",100.00, "SPIN","Account1", false,""],
-    ["2019-06-08T02:59:43.1Z", "DBIT",100.00, "SPIN","Account1", true,"FEE DR"],
+    ["2019-06-08T02:59:43.1Z", "DBIT", 100.0, "SPIN", "Account1", false, ""],
+    [
+      "2019-06-08T02:59:43.1Z",
+      "DBIT",
+      100.0,
+      "SPIN",
+      "Account1",
+      true,
+      "FEE DR"
+    ],
 
-    ["2019-06-09T02:59:43.1Z", "DBIT",100.00, "SPIN","Account1", false,""],
-    ["2019-06-09T02:59:43.1Z", "DBIT",100.00, "SPIN","Account1", true,"FEE CR"],
+    ["2019-06-09T02:59:43.1Z", "DBIT", 100.0, "SPIN", "Account1", false, ""],
+    [
+      "2019-06-09T02:59:43.1Z",
+      "DBIT",
+      100.0,
+      "SPIN",
+      "Account1",
+      true,
+      "FEE CR"
+    ],
 
     ["2019-05-01T02:59:43.1Z", "DBIT", 100.0, "SPOU", "Account1", false, ""],
     [
@@ -96,12 +175,20 @@ export class DataService {
       "FEE DR"
     ],
 
-    ["2019-06-03T02:59:43.1Z", "DBIT",100.00, "SPOU","Account1", false,""],
-    ["2019-06-02T02:59:43.1Z", "DBIT",100.00, "SPOU","Account1", true,"FEE CR"],
+    ["2019-06-03T02:59:43.1Z", "DBIT", 100.0, "SPOU", "Account1", false, ""],
+    [
+      "2019-06-02T02:59:43.1Z",
+      "DBIT",
+      100.0,
+      "SPOU",
+      "Account1",
+      true,
+      "FEE CR"
+    ],
 
-    ["2019-06-04T02:59:43.1Z", "DBIT",8.00, "","Account1", true,"A/C FEE"],
-    ["2019-06-05T02:59:43.1Z", "DBIT",44.17, "","Account1", true,"INT CR"],
-    ["2019-06-02T02:59:43.1Z", "DBIT",160.71, "","Account1", true,"INT DR"],
+    ["2019-06-04T02:59:43.1Z", "DBIT", 8.0, "", "Account1", true, "A/C FEE"],
+    ["2019-06-05T02:59:43.1Z", "DBIT", 44.17, "", "Account1", true, "INT CR"],
+    ["2019-06-02T02:59:43.1Z", "DBIT", 160.71, "", "Account1", true, "INT DR"],
 
     ["2019-05-01T02:59:43.1Z", "Dbit", 100.0, "TRF", "Account1", false, ""],
     [
@@ -125,11 +212,27 @@ export class DataService {
       "FEE CR"
     ],
 
-    ["2019-06-05T02:59:43.1Z", "DBIT",100.00, "IPO","Account2", false,""],
-    ["2019-06-09T02:59:43.1Z", "DBIT",100.00, "IPO","Account2", true,"FEE DR"],
+    ["2019-06-05T02:59:43.1Z", "DBIT", 100.0, "IPO", "Account2", false, ""],
+    [
+      "2019-06-09T02:59:43.1Z",
+      "DBIT",
+      100.0,
+      "IPO",
+      "Account2",
+      true,
+      "FEE DR"
+    ],
 
-    ["2019-06-08T02:59:43.1Z", "CRDT",100.00, "IPO","Account2", false,""],
-    ["2019-06-08T02:59:43.1Z", "DBIT",100.00, "IPO","Account2", true,"FEE CR"],
+    ["2019-06-08T02:59:43.1Z", "CRDT", 100.0, "IPO", "Account2", false, ""],
+    [
+      "2019-06-08T02:59:43.1Z",
+      "DBIT",
+      100.0,
+      "IPO",
+      "Account2",
+      true,
+      "FEE CR"
+    ],
 
     ["2019-05-01T02:59:43.1Z", "DBIT", 100.0, "PAY", "Account2", false, ""],
     [
@@ -273,52 +376,58 @@ export class DataService {
   transactionItems: TransactionItem[] = [];
 
   selectedAccount: string;
-  
-  constructor() { }
-  
+
+  constructor() {}
+
   MapMockData() {
     this.mockData = [];
-    
+
     this.crapData.forEach(x => {
       const tran = new Transaction();
       tran.Date = moment(x[0].toString()).toDate();
       tran.PaymentType =
-        [1].toString().toLowerCase() === "dbit"
+        x[1].toString().toLowerCase() === "dbit"
           ? TransactionType.Debit
           : TransactionType.Credit;
       tran.AMT = parseInt(x[2].toString());
       tran.FeeType = x[3] === "TRF" ? FeeType.TRF : FeeType.IPO;
       tran.Account = x[4].toString();
-      tran.Fee = x[3] === "TRF" ? 0.50 : 0.70;
+      tran.Fee = x[3] === "TRF" ? 0.5 : 0.7;
 
       this.mockData.push(tran);
     });
-    
-    if (this.selectedAccount == null) this.selectedAccount = this.mockData[0].Account;
+
+    if (this.selectedAccount == null)
+      this.selectedAccount = this.mockData[0].Account;
 
     return this.mockData;
   }
 
   GetTransactionItems(transactions: Transaction[]) {
     this.transactionItems = [];
-    
+
     let sevenDaysAgo = moment(new Date())
-    .subtract(7, "day")
-    .toDate();
+      .subtract(7, "day")
+      .toDate();
 
     while (sevenDaysAgo.getDate() !== new Date().getDate()) {
       const item = new TransactionItem();
 
       item.Day = moment(sevenDaysAgo).format("dddd");
       item.Transactions = transactions.filter(
-        x => (x.Date.getDate() == sevenDaysAgo.getDate()) && x.Account === this.selectedAccount
+        x =>
+          x.Date.getDate() == sevenDaysAgo.getDate() &&
+          x.Account === this.selectedAccount
       );
 
       item.Amount = item.Transactions.reduce((acc, currentValue) => {
         return (acc += currentValue.AMT);
       }, 0);
 
-      item.FeeAmount = item.Transactions.reduce((curr, next) => curr += (next.FeeType === FeeType.TRF ? 0.50 : 0.70), 0);
+      item.FeeAmount = item.Transactions.reduce(
+        (curr, next) => (curr += next.FeeType === FeeType.TRF ? 0.5 : 0.7),
+        0
+      );
 
       this.transactionItems.push(item);
 
@@ -327,7 +436,6 @@ export class DataService {
         .toDate();
     }
 
-    console.log(this.transactionItems);
     return this.transactionItems;
   }
 }
